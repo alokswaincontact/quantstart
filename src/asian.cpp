1a76afb9945971567d02e89c31722690abe72c5e
Alok Swain <alokswain1@gmail.com>
2025-06-28 21:00:13 -0500

    Fix compilation error
    /Users/alok/code/c_plus_plus/blog/quantstart/src/asian.cpp:21:51: error: use of class template 'vector' requires template arguments; argument deduction not allowed in function prototype
    double AsianOptionArithmetic::pay_off_price(const std::vector& spot_prices) const {
                                                      ^~~
    /Library/Developer/CommandLineTools/SDKs/MacOSX14.4.sdk/usr/include/c++/v1/vector:383:28: note: template is declared here
    class _LIBCPP_TEMPLATE_VIS vector
                               ^
    /Users/alok/code/c_plus_plus/blog/quantstart/src/asian.cpp:35:50: error: use of class template 'vector' requires template arguments; argument deduction not allowed in function prototype
    double AsianOptionGeometric::pay_off_price(const std::vector& spot_prices) const {
                                                     ^~~
    /Library/Developer/CommandLineTools/SDKs/MacOSX14.4.sdk/usr/include/c++/v1/vector:383:28: note: template is declared here
    class _LIBCPP_TEMPLATE_VIS vector
                               ^
    2 errors generated.

--- src/asian.cpp
+++ src/asian.cpp
@@ -18,7 +18,7 @@ AsianOption::AsianOption(PayOff* _pay_off) : pay_off(_pay_off) {}
 AsianOptionArithmetic::AsianOptionArithmetic(PayOff* _pay_off) : AsianOption(_pay_off) {}
 
 // Arithmetic mean pay-off price
-double AsianOptionArithmetic::pay_off_price(const std::vector& spot_prices) const {
+double AsianOptionArithmetic::pay_off_price(const std::vector<double>& spot_prices) const {
   unsigned num_times = spot_prices.size();
   double sum = std::accumulate(spot_prices.begin(), spot_prices.end(), 0);
   double arith_mean = sum / static_cast<double>(num_times);
@@ -32,7 +32,7 @@ double AsianOptionArithmetic::pay_off_price(const std::vector& spot_prices) cons
 AsianOptionGeometric::AsianOptionGeometric(PayOff* _pay_off) : AsianOption(_pay_off) {}
 
 // Geometric mean pay-off price
-double AsianOptionGeometric::pay_off_price(const std::vector& spot_prices) const {
+double AsianOptionGeometric::pay_off_price(const std::vector<double>& spot_prices) const {
   unsigned num_times = spot_prices.size();
   double log_sum = 0.0;
   for (int i=0; i<spot_prices.size(); i++) {
